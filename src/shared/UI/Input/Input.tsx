d4c587f26e779a5a642863406fdd96b947784667
BorysPresn <boryspresniak@gmail.com>
2025-07-01 14:28:43 +0200

    fix: restricted Input component types to prevent breakage

--- src/shared/UI/Input/Input.tsx
+++ src/shared/UI/Input/Input.tsx
@@ -1,42 +1,48 @@
-import React from "react";
+import React, { type InputHTMLAttributes, useId } from "react";
 
 import clsx from "clsx";
 
 import cl from "./Input.module.scss";
 
-interface IInputProps {
+type AllowedInputType = "text" | "password" | "email";
+
+interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  type: AllowedInputType;
   label?: string;
-  placeholder?: string;
-  type?: string;
-  inputId: string;
-  error: string;
+  inputId?: string;
+  error?: string;
   ref?: React.Ref<HTMLInputElement>;
 }
 const Input: React.FC<IInputProps> = ({
   label,
   inputId,
   placeholder,
-  type = "text",
+  type,
   error,
   ref,
   ...rest
-}) => (
-  <div className={clsx(cl.inputContainer, error && cl.error)}>
-    {label && (
-      <label htmlFor={inputId} className={cl.label}>
-        {label}
-      </label>
-    )}
-    <input
-      ref={ref}
-      id={inputId}
-      type={type}
-      placeholder={placeholder}
-      className={cl.input}
-      {...rest}
-    />
-    <p className={clsx(cl.error, error && cl.errorVisible)}>{error}</p>
-  </div>
-);
+}) => {
+  const uniqueId = useId();
+  const inpId = inputId || uniqueId;
+
+  return (
+    <div className={clsx(cl.inputContainer, error && cl.error)}>
+      {label && (
+        <label htmlFor={inpId} className={cl.label}>
+          {label}
+        </label>
+      )}
+      <input
+        ref={ref}
+        id={inpId}
+        type={type}
+        placeholder={placeholder}
+        className={cl.input}
+        {...rest}
+      />
+      <p className={clsx(cl.error, error && cl.errorVisible)}>{error}</p>
+    </div>
+  );
+};
 
 export default Input;
