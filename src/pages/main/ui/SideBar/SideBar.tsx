117978b5eebd7e6ab4c307f60959372e67dc93ce
BorysPresn <boryspresniak@gmail.com>
2025-07-01 12:21:16 +0200

    refactor: move variable pages out of function scope

--- src/pages/main/ui/SideBar/SideBar.tsx
+++ src/pages/main/ui/SideBar/SideBar.tsx
@@ -6,29 +6,28 @@ import SideBarLink from "./SideBarLink.tsx";
 
 import cl from "./SideBar.module.scss";
 
-const SideBar: React.FC = () => {
-  const pages: NavIconName[] = [
-    "dashboard",
-    "deals",
-    "customers",
-    "tasks",
-    "calendar",
-    "notifications",
-    "settings",
-  ];
-  return (
-    <aside className={cl.sidebar}>
-      <nav>
-        <ul className={cl.navList}>
-          {pages.map((page) => (
-            <li key={page}>
-              <SideBarLink iconName={page} />
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </aside>
-  );
-};
+const pages: NavIconName[] = [
+  "dashboard",
+  "deals",
+  "customers",
+  "tasks",
+  "calendar",
+  "notifications",
+  "settings",
+];
+
+const SideBar: React.FC = () => (
+  <aside className={cl.sidebar}>
+    <nav>
+      <ul className={cl.navList}>
+        {pages.map((page) => (
+          <li key={page}>
+            <SideBarLink iconName={page} />
+          </li>
+        ))}
+      </ul>
+    </nav>
+  </aside>
+);
 
 export default SideBar;
