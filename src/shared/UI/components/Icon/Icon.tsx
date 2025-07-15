c4f7d3943466c86f37eaabc7a1b1c0ef6431da42
BorysPresn <boryspresniak@gmail.com>
2025-07-15 19:15:14 +0200

    fix: corrected paths to styles and icon files

--- src/shared/UI/components/Icon/Icon.tsx
+++ src/shared/UI/components/Icon/Icon.tsx
@@ -1,25 +1,25 @@
 import React, { type FC, type SVGProps } from "react";
 
-import ToastFire from "../assets/emoji/ToastFire.svg?react";
-import ToastFirework from "../assets/emoji/ToastFireWork.svg?react";
-import CalendarBoldIcon from "../assets/icons/CalendarBoldIcon.svg?react";
-import CalendarIcon from "../assets/icons/CalendarIcon.svg?react";
-import CustomersBoldIcon from "../assets/icons/CustomersBoldIcon.svg?react";
-import CustomersIcon from "../assets/icons/CustomersIcon.svg?react";
-import DashboardBoldIcon from "../assets/icons/DashboardBoldIcon.svg?react";
-import DashboardIcon from "../assets/icons/DashboardIcon.svg?react";
-import DealsBoldIcon from "../assets/icons/DealsBoldIcon.svg?react";
-import DealsIcon from "../assets/icons/DealsIcon.svg?react";
-import LogoIcon from "../assets/icons/LogoIcon.svg?react";
-import NotificationsBoldIcon from "../assets/icons/NotificationsBoldIcon.svg?react";
-import NotificationsIcon from "../assets/icons/NotificationsIcon.svg?react";
-import SearchIcon from "../assets/icons/SearchIcon.svg?react";
-import SettingsBoldIcon from "../assets/icons/SettingsBoldIcon.svg?react";
-import SettingsIcon from "../assets/icons/SettingsIcon.svg?react";
-import TasksBoldIcon from "../assets/icons/TasksBoldIcon.svg?react";
-import TasksIcon from "../assets/icons/TasksIcon.svg?react";
-import ToastClose from "../assets/icons/ToastClose.svg?react";
-import AddIcon from "../assets/icons/addIcon.svg?react";
+import ToastFire from "../../assets/emoji/ToastFire.svg?react";
+import ToastFirework from "../../assets/emoji/ToastFireWork.svg?react";
+import CalendarBoldIcon from "../../assets/icons/CalendarBoldIcon.svg?react";
+import CalendarIcon from "../../assets/icons/CalendarIcon.svg?react";
+import CustomersBoldIcon from "../../assets/icons/CustomersBoldIcon.svg?react";
+import CustomersIcon from "../../assets/icons/CustomersIcon.svg?react";
+import DashboardBoldIcon from "../../assets/icons/DashboardBoldIcon.svg?react";
+import DashboardIcon from "../../assets/icons/DashboardIcon.svg?react";
+import DealsBoldIcon from "../../assets/icons/DealsBoldIcon.svg?react";
+import DealsIcon from "../../assets/icons/DealsIcon.svg?react";
+import LogoIcon from "../../assets/icons/LogoIcon.svg?react";
+import NotificationsBoldIcon from "../../assets/icons/NotificationsBoldIcon.svg?react";
+import NotificationsIcon from "../../assets/icons/NotificationsIcon.svg?react";
+import SearchIcon from "../../assets/icons/SearchIcon.svg?react";
+import SettingsBoldIcon from "../../assets/icons/SettingsBoldIcon.svg?react";
+import SettingsIcon from "../../assets/icons/SettingsIcon.svg?react";
+import TasksBoldIcon from "../../assets/icons/TasksBoldIcon.svg?react";
+import TasksIcon from "../../assets/icons/TasksIcon.svg?react";
+import ToastClose from "../../assets/icons/ToastClose.svg?react";
+import AddIcon from "../../assets/icons/addIcon.svg?react";
 
 export type NavIconName =
   | "dashboard"
